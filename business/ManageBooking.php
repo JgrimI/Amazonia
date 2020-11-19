fb4f544850a5bbf13e85cdabf7c56327217b282d
alejandro garcia <42383618+alejocrasi@users.noreply.github.com>
2020-11-19 12:59:21 -0500

    Booking clases

---
+++ business/ManageBooking.php
@@ -0,0 +1,75 @@
+<?php
+    /**
+     * Importe de clases
+     */
+    require_once($_SERVER["DOCUMENT_ROOT"]).'/Amazonia/persistence/util/Connection.php';
+    require_once($_SERVER["DOCUMENT_ROOT"]).'/Amazonia/persistence/BookingDAO.php';
+
+    class ManageBook{
+
+        /**
+         * Atributo para la conexión a la base de datos
+         */
+        private static $connection;
+
+        function __construct(){
+
+        }
+
+        /**
+         * Obtiene un bookistrador
+        * @param  [String] $book [Nombre de book del bookistrador a buscar]
+        * @return [bookistrador] bookistrador encontrado
+        */
+        public static function consult($cod_booking){
+
+            $bookingDAO=BookingDAO::getBookingDAO(self::$connection);
+            $booking=$bookingDAO->consult($cod_booking);
+            return $booking;
+
+        }
+        
+        /**
+         * Crea un nuevo booki
+         * @param book bookistrador a ingresar
+         * @return void
+         */
+        public static function create($booking){
+            $bookingDAO=bookingDAO::getBookingDAO(self::$connection);
+            $bookingDAO->create($booking);
+
+        }
+
+           /**
+         * Lista todos los bookistradores
+         * @return book[] Lista de todos los bookistradores de la base de datos
+         */
+        public  static function listAll(){
+            $bookingDAO = bookingDAO::getBookingDAO(self::$connection);
+            $bookings=$bookingDAO->listAll();
+            return $bookings;
+        }
+
+
+        /**
+         * Modifica un bookistrador
+         * @param book bookistrador a modificar
+         * @return void
+         */
+        public static function modify($booking){
+            $bookingDAO=bookingDAO::getBookingDAO(self::$connection);
+            $bookingDAO->modify($booking);
+        }
+
+
+
+        /**
+         * Cambia la conexión
+         */
+        public static function setConnectionBD($connection)
+        {
+            self::$connection = $connection;
+        }
+    }
+
+?>
---
+++ persistence/BookingDAO.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Archivo de conexión a la base de datos
+ */
+require_once('../persistence/util/Connection.php');
+
+/**
+ * Archivo de entidad
+ */
+require_once('../business/Booking.php');
+/**
+ * Interfaz DAO
+ */
+require_once('DAO.php');
+
+/**
+ * Books DAO
+ */
+class BookingDAO implements DAO
+{
+	/**
+	 * Conexión a la base de datos
+	 * @var [Object]
+	 */
+	private $connection;
+
+	/**
+	 * Objeto de la clase bookDAO
+	 * @var [bookDAO]
+	 */
+	private static $bookingDAO;
+
+	/**
+	 * Constructor de la clase
+	 */
+
+	private function __construct($connection)
+	{
+		$this->connection=$connection;
+	}
+
+	/**
+	 * Realiza la consulta de un objeto
+	 * @param  [int] $codigo [Código del objeto a consultar]
+	 * @return [book] book encontrado
+	 */
+	public function consult($cod_booking){
+		$query="SELECT * FROM booking WHERE cod_booking=".$cod_booking;
+		$rs = pg_query( $this->connection, $query );
+		$booking = new Booking();
+		if( $rs )
+        {
+             if( pg_num_rows($rs) > 0 )
+            {
+				$obj = pg_fetch_object($rs,0 );
+				$booking->setCod_booking($obj->cod_booking);
+				$booking->setCod_document($obj->cod_document);
+				$booking->setType_document($obj->type_document);
+				$booking->setAvailable($obj->available);
+				$booking->setCod_user($obj->cod_user);
+            }
+            
+		}
+		return $booking;
+	}
+
+	/**
+	 * Crea una nuevo book en la base de datos
+	 * @param  book $newBook
+	 * @return void
+	 */
+	public function create ($newBooking){
+		
+		$query="INSERT INTO booking VALUES(0,'".$newBooking->getCod_document()."','".$newBooking->getType_document()."','".$newBooking->getAvailable()."','".$newBooking->getCod_user()."');";
+		pg_query($this->connection, $query);
+
+	}
+
+	/**
+	 * Modifica una book ingresado por parámetro
+	 * @param  book $book book ingresado por parámetro
+	 * @return void
+	 */
+	public function modify ($booking){
+
+		$query="UPDATE booking SET cod_document='".$booking->getCod_document()."', type_document ='".$booking->getType_document()."', available ='".$booking->getAvailable()."', cod_user='".$booking->getCod_user()."' ;";
+		pg_query($this->connection,$query);
+	}
+
+	/**
+	 * Lista todos los objetos que se están en la tabla de book
+	 * @return [books]
+	 */
+	public function listAll(){
+		$query="SELECT * FROM booking";
+		$rs = pg_query( $this->connection, $query );
+		$bookings = array();
+		if( $rs ){
+			if( pg_num_rows($rs) > 0 ){
+			   while( $obj = pg_fetch_object($rs) ){
+					$booking=new booking();
+			                    
+					$booking->setCod_booking($obj->cod_booking);
+				    $booking->setCod_document($obj->cod_document);
+				    $booking->setType_document($obj->type_document);
+				    $booking->setAvailable($obj->available);
+				    $booking->setCod_user($obj->cod_user);
+					
+					array_push($bookings,$booking);
+			   }
+			}
+		}
+		return $bookings;
+	}
+
+	/*
+	*Obtiene el objeto de esta clase
+	*
+	*@param $conexion
+	*@return void
+	*/
+	public static function getBookingDAO($connection) {
+            if(self::$bookingDAO == null) {
+                self::$bookingDAO = new bookingDAO($connection);
+            }
+
+            return self::$bookingDAO;
+    }
+
+}
