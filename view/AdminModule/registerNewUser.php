53194ce795f3adf44ba623e2f767349aa89d4193
alejandro garcia <42383618+alejocrasi@users.noreply.github.com>
2020-11-24 19:32:34 -0500

    Update registerNewUser.php

--- view/AdminModule/registerNewUser.php
+++ view/AdminModule/registerNewUser.php
@@ -11,33 +11,69 @@ if (isset($_POST['add'])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
     $name = $_POST['name'];
+    $tipo = $_POST['tipo'];
+  
+    if($tipo == 'admin'){
+        ManageAdmin::setConnectionBD($connection);
+        $validAdmin = ManageAdmin::consultByMail($email);
 
-    ManageUser::setConnectionBD($connection);
-    $validUser = ManageUser::consultByMail($email);
-    if ($validUser->getId() != '') {
-        echo printMessage("Error!!", "The mail is already registered", "error");
-    } else {
-        $user = new User();
-        $user->setEmail($email);
-        $user->setPassword($password);
-        $user->setName($name);
-        $user->setStatus('inactivo');
+        if ($validAdmin->getId() != '') {
+            echo printMessage("Error!!", "The mail is already registered", "error");
+        } else {
+            $admin = new Administrator();
+            $admin->setEmail($email);
+            $admin->setPassword($password);
+            $admin->setName($name);
+            $admin->setStatus('inactivo');
+    
+            ManageAdmin::createAdmin($admin);
+    
+            $admin = ManageAdmin::consultByMail($email);
+            sendMail($email, $name, $admin->getId());
+    
+            echo printMessage("Congratulations", "your account was created successfully", "success");
+        }
+        $con->turnOffBD($connection);       
+    }else if($tipo == 'user'){
 
-        ManageUser::createUser($user);
-
-        $user = ManageUser::consultByMail($email);
-        sendMail($email, $name, $user->getId());
-
-        echo printMessage("Congratulations", "your account was created successfully", "success");
+        ManageUser::setConnectionBD($connection);
+        $validUser = ManageUser::consultByMail($email);
+        if ($validUser->getId() != '') {
+            echo printMessage("Error!!", "The mail is already registered", "error");
+        } else {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setPassword($password);
+            $user->setName($name);
+            $user->setStatus('inactivo');
+    
+            ManageUser::createUser($user);
+    
+            $user = ManageUser::consultByMail($email);
+            sendMail($email, $name, $user->getId());
+    
+            echo printMessage("Congratulations", "your account was created successfully", "success");
+        }
+        $con->turnOffBD($connection);       
     }
-    $con->turnOffBD($connection);
 }
 
 ?>
 <style>
-    .select-styled {
-        display: none;
+    
+    .navbar-default .navbar-nav>.home>a,
+    .navbar-default .navbar-nav>.home>a:hover,
+    .navbar-default .navbar-nav>.home>a:focus {
+        color: #ff7236;
+        background-color: transparent;
     }
+
+    .xd {
+        background-color: transparent !important;
+        height: 50px;
+    }
+
+    
 </style>
 <!-- Start: Page Banner -->
 <section class="page-banner services-banner">
@@ -61,11 +97,10 @@ if (isset($_POST['add'])) {
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
             <div class="signin-main">
-                <div class="container">
+                <div class="container">                                
                     <div class="woocommerce">
                         <div class="woocommerce-login">
-                            <div class="company-info signin-register">
-                                
+                            <div class="company-info signin-register">                               
                                 <div class="col-md-5 border-dark new-user">
                                     <div class="row">
                                         <div class="col-md-12">
@@ -78,7 +113,10 @@ if (isset($_POST['add'])) {
                                                     <input type="text" id="name" name="name" class="input-text" required placeholder="Name" style="margin-top:4%;">
                                                     <input type="text" id="email" name="email" class="input-text" required placeholder="Email">
                                                     <input type="password" id="password" name="password" class="input-text" required placeholder="Password">
-                                                    <div class="clear"></div>
+                                                    <select id="tipo" name="tipo" class="tipo" style="width:100%;" required >
+                                                        <option value="admin">Administrador</option>
+                                                        <option value="user">Usuario</option>
+                                                    </select>                                                          
                                                     <input type="submit" value="add" name="add" id="add" class="button btn btn-default">
                                                     <div class="clear"></div>
                                                 </form>
