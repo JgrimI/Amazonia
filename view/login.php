3d101f9c3f44634b2b194c2a56a952e1d1660736
jleonf2020 <61398900+jleonf2020@users.noreply.github.com>
2020-11-27 07:20:24 -0500

    Update login.php

--- view/login.php
+++ view/login.php
@@ -100,7 +100,7 @@ if (isset($_GET['i'])) {
         <div class="breadcrumb">
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li>Sign In</li>
+                <li>Sign in</li>
             </ul>
         </div>
     </div>
